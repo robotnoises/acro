7530a580a53a61518447eb798df1aeecd36f5262
David Nichols <dnich@Davids-MacBook-Pro-2.local>
2016-02-10 21:28:55 -0500

    More models

--- data/models.js
+++ data/models.js
@@ -1,6 +1,32 @@
 var Sequelize = require('sequelize');
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Globals /////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var GAME_STATES = [
+  'INPROG', 
+  'COMPLETE'
+];
+
+var ROUND_STAGES = [
+  'ROUND_1', 
+  'ROUND_2', 
+  'ROUND_3', 
+  'ROUND_4', 
+  'ROUND_5', 
+  'ROUND_6', 
+  'FACEOFF_1', 
+  'FACEOFF_2', 
+  'FACEOFF_3'
+];
+
+var SCORE_BONUSES = [
+  'NONE',
+  'SPEED'
+];
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Model definitions ///////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -8,14 +34,20 @@ var User = {
   user_name: Sequelize.STRING
 };
 
-var Room = {
-  room_name: Sequelize.STRING
+var Game = {
+  state: Sequelize.ENUM(GAME_STATES),
+  winner: Sequelize.INTEGER
 };
 
-var Game = {
-  state: Sequelize.ENUM('INPROG', 'COMPLETE'),
-  winner: Sequelize.INTEGER
-  // round_1: 
+var Round = {
+  stage: Sequelize.ENUM(ROUND_STAGES),
+  acronym: Sequelize.ARRAY,
+  validator: Sequelize.STRING
+};
+
+var Score = {
+  score: Sequelize.INTEGER,
+  bonus: Sequelize.ENUM(SCORE_BONUSES)
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -24,7 +56,7 @@ var Game = {
 
 function _define(connection, modelName, model) {
   try {
-    connection.define(modelName, model);  
+    return connection.define(modelName, model);  
   } catch (ex) {
     console.error(ex);
   }
@@ -37,9 +69,19 @@ function _define(connection, modelName, model) {
 function _defineAll(connection) {
   
   if (connection && connection.define) {
-    _define(connection, 'user', User);
-    _define(connection, 'room', Room);
-    _define(connection, 'game', Game);
+    
+    // Define each Model
+    
+    var user = _define(connection, 'user', User);
+    var game = _define(connection, 'game', Game);
+    var round = _define(connection, 'round', Round);
+    var score = _define(connection, 'score', Score);
+    
+    // Define Model Associations
+    
+    // user.belongsTo(game);
+    // round.belongsTo(game);
+    // score.hasMany(user, round);
   } else {
     console.error('A valid Sequelize connection is required.');
   }
